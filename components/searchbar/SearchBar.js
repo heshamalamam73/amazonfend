762bb9e624280764c8f50a32a3a1e5e38b985e40
mahmoud hassan <heshamalamam73@gmail.com>
2021-01-22 18:03:13 +0100

    edit some styles e finish v1

--- components/searchbar/SearchBar.js
+++ components/searchbar/SearchBar.js
@@ -2,17 +2,10 @@
 
 import { useEffect, useState } from "react";
 import SearchList from "../searchList/SearchList";
-import styles from "./SearchBar.module.css";
 function SearchBar({ countries }) {
   return (
     <>
-      <input
-        className={styles.SearchBar}
-        type="text"
-        placeholder="Search"
-        id="input"
-        autoComplete="false"
-      />
+      <input type="text" placeholder="Search" id="input" autoComplete="false" />
     </>
   );
 }
